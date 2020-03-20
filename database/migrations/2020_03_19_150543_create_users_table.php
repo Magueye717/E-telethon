3a8e3e1da7e7ff1057b2be4c94b4882cf991a685
Magueye <magueye717@gmail.com>
2020-03-20 11:12:21 -0700

    correction modelisation

--- database/migrations/2020_03_19_150543_create_users_table.php
+++ database/migrations/2020_03_19_150543_create_users_table.php
@@ -2,25 +2,36 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
-
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('nom');
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
 			$table->string('prenom');
-			$table->string('email');
-			$table->string('password');
-			$table->string('adresse')->nullable();
-			$table->rememberToken('rememberToken');
-			$table->timestamps();
-		});
-	}
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
 
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
